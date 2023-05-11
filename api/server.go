2709a25b29ba9b9f3ff033c868e7aab271153473
Takara Truong <takaraktruong@gmail.com>
2023-05-11 14:14:18 -0700

    created custom currency validator

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/truont2/simplebank/db/sqlc"
 )
 
@@ -18,11 +20,16 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	// routes
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
 	router.PUT("/accounts", server.updateAccount)
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -91,6 +91,7 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 			AccountID: arg.ToAccountID,
 			Amount:    arg.Amount,
 		})
+
 		if err != nil {
 			return err
 		}
@@ -106,7 +107,7 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 		}
 
-		return nil
+		return err
 	})
 
 	return result, err
